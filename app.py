178532cf13863406105ae6ce8d7991e0ef289586
hieu <hieuchi911@gmail.com>
2022-07-03 10:51:35 +0700

    load model from huggingface and cache to models/ folder instead of mannually download models in advance; remove print's

--- app.py
+++ app.py
@@ -9,35 +9,26 @@ app = Sanic("BBOT-Server")
 async def load_model(app):
     global MODEL, TOKENIZER
     # Initialize the tokenizer, model for Blender Bot
-    # TOKENIZER = AutoTokenizer.from_pretrained("models/blenderbot-400M-distill")
-    # MODEL = AutoModelForSeq2SeqLM.from_pretrained("models/blenderbot-400M-distill")
+    # TOKENIZER = AutoTokenizer.from_pretrained("facebook/blenderbot-400M-distill",
+    #                                         cache_dir="models/blenderbot-400M-distill")
+    # MODEL = AutoModelForSeq2SeqLM.from_pretrained("facebook/blenderbot-400M-distill",
+    #                                         cache_dir="models/blenderbot-400M-distill")
 
-    TOKENIZER = AutoTokenizer.from_pretrained("models/blenderbot_small-90M")
-    MODEL = AutoModelForSeq2SeqLM.from_pretrained("models/blenderbot_small-90M")
+    TOKENIZER = AutoTokenizer.from_pretrained("facebook/blenderbot_small-90M",
+                                            cache_dir="models/blenderbot_small-90M")
+    MODEL = AutoModelForSeq2SeqLM.from_pretrained("facebook/blenderbot_small-90M",
+                                            cache_dir="models/blenderbot_small-90M")
 
 @app.route('/open-chat', methods=['POST'])
 async def en_to_vi(request):
     global MODEL, TOKENIZER
     text = request.json["text"]
 
-    start = time.time()
     tokenized_text = TOKENIZER([text], return_tensors="pt")
-    end = time.time()
-    print("\n\nTOKENIZER took: ", end - start)
 
     # Perform translation and decode the output
-    start = time.time()
     reply_ids = MODEL.generate(**tokenized_text)
-    end = time.time()
-    print("\nMODEL generate took: ", end - start)
-    
-    start = time.time()
     reply_text = TOKENIZER.batch_decode(reply_ids, skip_special_tokens=True)[0]
-    end = time.time()
-    print("\nTOKNEIZER batch decode took: ", end - start)
-
-    # Print translated text
-    print("\n", reply_text, "\n\n")
 
     return json({'text': reply_text})
 
